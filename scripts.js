26de5545337dc9a55d10cdba11366de4c4530f90
Ea Francisco <emberkasai04@gmail.com>
2022-02-07 16:42:16 -0500

    add kanji API

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const gameEl = document.getElementById("game")
+const getGame = document.getElementById("get-game")
+
+getGame.addEventListener("click", function() {
+    getGameData();
+
+})
+
+function getGameData(){
+
+    fetch("https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/?ks=16", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "kanjialive-api.p.rapidapi.com",
+            "x-rapidapi-key": "a7cd65d8f5msh29d7c6f25510201p18afacjsn0851cc9ec915"
+        }
+    })
+    .then((response) => response.json())
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+}
